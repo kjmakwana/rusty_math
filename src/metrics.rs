c8b7c7e4c5e9507a9e05091940a8ae5930fb042a
Kshitij Makwana <kjmakwana00@gmail.com>
2024-08-26 10:42:21 +0530

    add recall and f1

--- src/metrics.rs
+++ src/metrics.rs
@@ -6,6 +6,7 @@
 //! - accuracy
 //! - confusion_matrix
 //! - precision
+//! - recall
 
 use std::collections::{HashMap, HashSet};
 
@@ -168,7 +169,7 @@ pub fn precision(y_pred: &Vec<u8>, y_true: &Vec<u8>, method: &str) -> HashMap<St
     let mut p = 0.0;
     let mut precision = HashMap::new(); 
     if nunique==1 {
-        precision.insert("precision".to_string(), matrix[0][0] as f64 / (matrix[0][0] + matrix[0][1]) as f64);
+        precision.insert("overall".to_string(), matrix[0][0] as f64 / (matrix[0][0] + matrix[0][1]) as f64);
     }
     else {
         
@@ -204,6 +205,97 @@ pub fn precision(y_pred: &Vec<u8>, y_true: &Vec<u8>, method: &str) -> HashMap<St
     
 
 
+/// Calculate the recall of a classification model. The recall is defined as the number of true positive predictions divided by the total number of actual positive values.
+/// Function calculates the overall recall and the recall for each class (in case of multiclass classification). The function supports two methods for calculating recall: "macro" and "micro".
+/// # Parameters
+/// y_pred: &Vec<u8> - A reference to a vector containing the predicted target values
+/// y_true: &Vec<u8> - A reference to a vector containing the true target values
+/// method: &str - The method to use for calculating recall. Options are "macro" and "micro"
+/// # Returns
+/// HashMap<String,f64> - A hashmap containing the overall recall and the recall for each class
+/// # Examples
+/// ```
+/// use rusty_math::metrics::recall;
+/// let y_pred = vec![0, 1, 1, 0, 1, 0, 1, 1, 1, 0];
+/// let y_true = vec![0, 1, 1, 0, 1, 0, 1, 0, 1, 0];
+/// let recall = recall(&y_pred, &y_true, "macro");
+/// ```
+/// # Panics
+/// If the number of samples in the predicted values does not match the number of samples in the true values
+/// If the method is not "macro" or "micro"
+pub fn recall(y_pred: &Vec<u8>,y_true: &Vec<u8>,method: &str) -> HashMap<String,f64> {
+    if y_pred.len() != y_true.len() {
+        panic!("Number of samples in predicted values does not match the number of samples in true values");
+    }
+    let matrix = confusion_matrix(&y_pred, &y_true);
+    let nunique = y_true.iter().collect::<HashSet<_>>().len();
+    let mut r = 0.0;
+    let mut recall = HashMap::new(); 
+    if nunique==1 {
+        recall.insert("overall".to_string(), matrix[0][0] as f64 / (matrix[0][0] + matrix[1][0]) as f64);
+    }
+    else {
+        
+        match method {
+            "macro" => {
+                let mut r_sum = 0.0;
+                for i in 0..nunique {
+                    for j in 0..nunique{
+                        r+=matrix[j][i] as f64;
+                    }
+                    r_sum+=matrix[i][i] as f64;
+                    recall.insert(format!("recall_{}",i), matrix[i][i] as f64 / r as f64);
+                }
+                recall.insert("overall".to_string(), r_sum as f64 / nunique as f64);
+            }
+            "micro" => {
+                let mut tp = 0;
+                let mut f_n = 0;
+                for i in 0..nunique {
+                    tp+=matrix[i][i];
+                    for j in 0..nunique {
+                        f_n+=matrix[i][j];
+                    }
+                    recall.insert(format!("recall_{}",i), tp as f64 / (tp + f_n) as f64);
+                }
+                recall.insert("overall".to_string(), tp as f64 / (tp + f_n) as f64);
+            }
+            _ => panic!("Invalid method")
+        }
+    }
+    recall
+}
+
+
+/// Calculate the F1 score of a classification model. The F1 score is the harmonic mean of the precision and recall.
+/// Function calculates the overall F1 score and the F1 score for each class (in case of multiclass classification). The function supports two methods for calculating F1 score: "macro" and "micro".
+/// # Parameters
+/// y_pred: &Vec<u8> - A reference to a vector containing the predicted target values
+/// y_true: &Vec<u8> - A reference to a vector containing the true target values
+/// method: &str - The method to use for calculating F1 score. Options are "macro" and "micro"
+/// # Returns
+/// HashMap<String,f64> - A hashmap containing the overall F1 score and the F1 score for each class
+/// # Examples
+/// ```
+/// use rusty_math::metrics::f1;
+/// let y_pred = vec![0, 1, 1, 0, 1, 0, 1, 1, 1, 0];
+/// let y_true = vec![0, 1, 1, 0, 1, 0, 1, 0, 1, 0];
+/// let f1 = f1(&y_pred, &y_true, "macro");
+/// ```
+/// # Panics
+/// If the number of samples in the predicted values does not match the number of samples in the true values
+/// If the method is not "macro" or "micro"
+pub fn f1(y_pred: &Vec<u8>,y_true: &Vec<u8>,method: &str) -> HashMap<String,f64> {
+    let precision = precision(&y_pred, &y_true, method);
+    let recall = recall(&y_pred, &y_true, method);
+    let nunique = y_true.iter().collect::<HashSet<_>>().len();
+    let mut f1 = HashMap::new();
+    f1.insert("overall".to_string(), 2.0 * (precision.get("overall").unwrap() * recall.get("overall").unwrap()) / (precision.get("overall").unwrap() + recall.get("overall").unwrap()));
+    for i in 0..nunique {
+        f1.insert(format!("f1_{}",i), 2.0 * (precision.get(&format!("precision_{}",i)).unwrap() * recall.get(&format!("recall_{}",i)).unwrap()) / (precision.get(&format!("precision_{}",i)).unwrap() + recall.get(&format!("recall_{}",i)).unwrap()));
+    }
+    f1
+}
 
 
 #[cfg(test)]
